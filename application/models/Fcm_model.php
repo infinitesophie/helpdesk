f2c341fa89a198ba9482ac972c7771a9a33a9eff
Ananth <anan.busyguy007@gmail.com>
2019-10-30 21:36:30 +0100

    Add select query

--- application/models/Fcm_model.php
+++ application/models/Fcm_model.php
@@ -29,6 +29,21 @@ class FCM_Model extends CI_Model
 		#  $this->db->where("user", $user)
         #	->set("token", $token)->update("fcm_user");
 	}
+
+	public function get_recent_articles($limit) 
+	{
+		return $this->db->select("knowledge_articles.ID, knowledge_articles.title,
+			knowledge_articles.last_updated_timestamp, knowledge_articles.body,
+			knowledge_articles.catid,
+			users.ID as userid, users.username, users.avatar, users.online_timestamp,
+			knowledge_categories.name as catname, knowledge_categories.image")
+			->join("knowledge_categories", "knowledge_categories.ID = knowledge_articles.catid")
+			->join("users", "users.ID = knowledge_articles.userid")
+			->order_by("knowledge_articles.ID", "DESC")
+			->limit($limit)
+			->get("knowledge_articles");
+
+	}
 }
 
 ?>	
